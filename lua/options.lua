e8c80633853d349f6975bb5fa2e82b8276c05e6e
Txema Otero <txedav@gmail.com>
2023-06-25 10:40:24 +0200

    Changed tabstop to 2

--- lua/options.lua
+++ lua/options.lua
@@ -58,14 +58,6 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = {"html", "vue", "json", "js"},
-    callback = function()
-        vim.opt.tabstop = 2
-        vim.opt.shiftwidth = 2
-        vim.opt.softtabstop = 2
-    end,
-})
 vim.opt.list = true
 vim.opt.listchars = "tab:->,trail:·"
 
