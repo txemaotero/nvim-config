be449d6b635ffb8d0c556f0a4702f6bf4b916228
Txema Otero <txedav@gmail.com>
2023-07-31 16:11:49 +0200

    Removed s config from flash

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,17 +1,8 @@
 return {
     "folke/flash.nvim",
     event = "VeryLazy",
-    ---@type Flash.Config
     opts = {},
     keys = {
-        {
-            "s",
-            mode = { "n", "x", "o" },
-            function()
-                require("flash").jump()
-            end,
-            desc = "Flash",
-        },
         {
             "S",
             mode = { "n", "o", "x" },
